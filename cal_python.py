464fb86542e181e28f38c587d34d29cf27487ee0
kulatav <kulatakshay@gmail.com>
2017-04-29 14:07:04 +0530

    adding correct program

--- cal_python.py
+++ cal_python.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 
-class calculator;
-def__init__(self);
-pass
+class calculator:
+    def __init__(self):
+        pass
 
-def add(self,x,y);
-return x+y
+    def add(self,x,y):
+        return x+y
 
-def sub(self,x,y);
-return x-y
+    def sub(self,x,y):
+        return x-y
+
+    def mult(self,x,y):
+        return x*y
 
 cal=calculator()
 print(cal.add(2,3))
 print(cal.sub(2,3))
+print(cal.mult(2,3))
